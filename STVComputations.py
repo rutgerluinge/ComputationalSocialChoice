e86508e41aefd918991a0f2710f282f59fa044e0
bandoos <l.bandelli@student.rug.nl>
2023-03-06 11:28:42 +0100

    refactoring, declarative tests, readme

--- STVComputations.py
+++ STVComputations.py
@@ -1,21 +1,39 @@
 from dataclasses import dataclass
+from typing import List, Dict, Set
 import re
+import itertools
+from copy import deepcopy, copy
+from pprint import pprint
 
 
 @dataclass
 class Profile:
-    """class to contain every row of data """
-    ballot: list[list[int]]     # [[1], [2], [3], [4,5], [6]]   : 1 > 2 > 3 > 4 = 5 > 6 (thats why 2d list)
+    """class to contain every row of data"""
+
+    # [[1], [2], [3], [4,5], [6]]   : 1 > 2 > 3 > 4 = 5 > 6 (thats why 2d list)
+    ballot: List[List[int]]
     count: int
+
+    def alts(self) -> Set[int]:
+        "Shortcut to get the set of alts in this Profile"
+        return set(itertools.chain(*self.ballot))
+
+
+def all_alts(ps: List[Profile]) -> Set[int]:
+    "Shortcut to get the overall set of alts in a list of Profile"
+    alts = set()
+    for p in ps:
+        alts.update(p.alts())
+    return alts
 
 
 def format_ballot(ballot: str) -> list:
     """extract good format for ballot from a string"""
 
     my_list = []
-    for x in re.findall('\d+|\{[^}]*\}', ballot):
-        if '{' in x:
-            sub_list = [int(y) for y in x.strip('{}').split(',')]
+    for x in re.findall(r"\d+|\{[^}]*\}", ballot):
+        if "{" in x:
+            sub_list = [int(y) for y in x.strip("{}").split(",")]
             my_list.append(sub_list)
         else:
             my_list.append([int(x)])
@@ -35,82 +53,124 @@ def format_ballot(ballot: str) -> list:
 #     return ballot_int
 
 
+def line_extract(line: str) -> Profile:
+    "Build Profile from `toi` text line"
+    data_parts = line.split(":")  # split count from ballot
+    count = int(data_parts[0])
+    ballot = data_parts[1]
+    ballot = format_ballot(ballot)
+    return Profile(ballot, count)
 
-def extract_data() -> list[Profile]:
-    """ function to read and extract data from the dataset
-        :return dictionary containing nr of votes as key, and ballot as value """
+
+def extract_data(path: str = "dataset_revised.txt") -> List[Profile]:
+    """function to read and extract data from the dataset
+    :return dictionary containing nr of votes as key, and ballot as value"""
 
     votes = list()
-    with open("dataset_revised.txt", "r") as file:
-        for line in file:
-            data_parts = line.split(":")  # split count from ballot
-            count = int(data_parts[0])
-            ballot = data_parts[1]
-            ballot = format_ballot(ballot)
-
-            votes.append(Profile(ballot, count))
-
-    return votes
+    with open(path, "r") as file:
+        return [
+            line_extract(line)
+            for line in file
+            if not line.startswith("#")
+            or not line.strip()  # skip comments and empty lines
+        ]
 
 
-def plurality_round(votes: list[Profile], available_alternatives: list[int]) -> dict[int, float]:
-    """does 1 round of plurality, then returns a dictionary containing alternative:nr of votes (plurality) """
+def plurality_round(
+    votes: List[Profile], available_alternatives: Set[int]
+) -> Dict[int, float]:
+    """does 1 round of plurality, then returns a dictionary containing alternative:nr of votes (plurality)"""
+
     alternative_count = dict()
     for alternative in available_alternatives:
         alternative_count[alternative] = 0
 
     for profile in votes:
-        if len(profile.ballot[0]) == 1:  # should not be doing for [{x,y},z,w]
-            alternative_count[profile.ballot[0][0]] += profile.count
+        # NOTE: we can do this version any way, still works if
+        # len(profile.ballot[0]) == 1, then divisor is 1 and we add the simple count
+        # if it's '{x,y,..}' case then we split the count equally
+        for idx, alt in enumerate(profile.ballot[0]):
+            alternative_count[profile.ballot[0][idx]] += profile.count * (
+                1 / len(profile.ballot[0])
+            )
 
-        if len(profile.ballot[0]) > 1:  # in case [{1, 2}, 3, 4] -> both 1 and 2 should get 0.5 point per vote
-            for idx, alt in enumerate(profile.ballot[0]):
-                alternative_count[profile.ballot[0][idx]] += profile.count * (1/len(profile.ballot[0]))
+        # TODO: remove if you agree with single implem
+        # if len(profile.ballot[0]) == 1:  # should not be doing for [{x,y},z,w]
+        #     alternative_count[profile.ballot[0][0]] += profile.count
+
+        # if (
+        #     len(profile.ballot[0]) > 1
+        # ):  # in case [{1, 2}, 3, 4] -> both 1 and 2 should get 0.5 point per vote
+        #     for idx, alt in enumerate(profile.ballot[0]):
+        #         alternative_count[profile.ballot[0][idx]] += profile.count * (
+        #             1 / len(profile.ballot[0])
+        #         )
+        # =====
 
     return alternative_count
 
 
-def remove_alternative(vote_profile: list[Profile], alternatives_to_remove: list[int]) -> list[Profile]:
+def remove_alternative(
+    vote_profile: List[Profile], alternatives_to_remove: Set[int]
+) -> List[Profile]:
+    """Removes alts from the given alt set from all the given profiles.
+    NOTE: makes in-place changes to the given profiles
+    """
+
     for alternative in alternatives_to_remove:  # in case multiple
+        # for each linear order
         for vote in vote_profile.copy():  # needs copy as we are removing from list
+            # and every cell in it
             for alt_ballot in vote.ballot:  # this will almost always run once  {1,2}
-                if alternative in alt_ballot: #CHECK if alternative matches ballot index
+                if (
+                    alternative in alt_ballot
+                ):  # CHECK if alternative matches ballot index
                     alt_ballot.remove(alternative)
+                    # if nothing remains, then delete
                     if len(alt_ballot) == 0:
                         vote.ballot.remove(alt_ballot)
 
+            # if the lin.order is left with 0 cells then delete
             if len(vote.ballot) == 0:  # remove empty ballots (no longer necessary)
                 vote_profile.remove(vote)
 
     return vote_profile
 
 
-def print_recap(p_scores: dict[int, float], alternatives: list[int], vote_round: int) -> None:
-    print(f"____________________________ vote round: {vote_round} ________________________________________\n"
-          f"plurality scores: {p_scores}\n"
-          f"alternatives to be removed: {alternatives}\n"
-          f"___________________________________________________________________________________\n")
+def print_recap(
+    p_scores: Dict[int, float], alternatives: Set[int], vote_round: int
+) -> None:
+    print(
+        f"\t____________________________ vote round: {vote_round} ________________________________________\n"
+        f"\tplurality scores: {p_scores}\n"
+        f"\talternatives to be removed: {alternatives}\n"
+        f"\t___________________________________________________________________________________\n"
+    )
 
 
-def stv_computations(votes:list[Profile], nr_of_alt:int, printing:bool) -> list[int]:
+def stv_computations(votes: List[Profile], nr_of_alt: int, printing: bool) -> List[int]:
     """STV algorithm:
     - calculate plurality scores
     - remove alternative with lowest alternative score (in case of a tie remove both)
     @:return list of integers containing the winner(s) of the vote
     """
-    all_alternatives = [x for x in range(1,nr_of_alt+1)]  # hardcoded for now
+    all_alternatives = [x for x in range(1, nr_of_alt + 1)]  # hardcoded for now
     vote_round = 1
 
     while vote_round < 12:
-        p_scores = plurality_round(votes=votes, available_alternatives=all_alternatives)
+        p_scores = plurality_round(
+            votes=votes, available_alternatives=set(all_alternatives)
+        )
 
         min_value = min(p_scores.values())
         alternatives = [key for key, value in p_scores.items() if value == min_value]
 
         if printing:
-            print_recap(p_scores, alternatives, vote_round)
+            print_recap(p_scores, set(alternatives), vote_round)
 
-        votes = remove_alternative(vote_profile=votes, alternatives_to_remove=alternatives)
+        votes = remove_alternative(
+            vote_profile=votes, alternatives_to_remove=set(alternatives)
+        )
 
         for alt in alternatives:
             all_alternatives.remove(alt)
@@ -122,6 +182,66 @@ def stv_computations(votes:list[Profile], nr_of_alt:int, printing:bool) -> list[
 
     return [0]
 
-if __name__ == '__main__':
-    votes = extract_data()
-    print(f"winner: {stv_computations(votes, 11, printing=True)}")
+
+def stv(votes: List[Profile], verbose: bool = False) -> Set[int]:
+    """
+    Slightly changed stv computation function.
+    - Auto computes alternatives from the given List[Profile]
+    - Does not modify the input objects, creates a deepcopy
+    - Uses and returns sets instead of lists (should be faster too)
+    - Loops untill all alts are removed and returns last non-empty alt-set instead of fixed # of rounds
+    """
+
+    full_alts = all_alts(votes)  # extract possible alternatives from ballots
+    round = 1
+
+    if verbose:
+        print("STV start: init_alts =", full_alts)
+
+    _alts_hist = []
+    _votes = deepcopy(votes)  # don't modify original votes
+    _alts = full_alts.copy()  # the working set of alts
+
+    if not _alts:
+        raise ValueError("There are no alternatives...")
+
+    while _alts:  # as long as there are alternatives left
+
+        _alts_hist.append(_alts.copy())  # store history of remaining alternatives
+
+        p_scores = plurality_round(_votes, _alts)  # run plurality round
+
+        min_value = min(p_scores.values())  # find minimal score
+        # find alts with minimal score
+        min_alts = set([k for k, v in p_scores.items() if v == min_value])
+
+        remove_alternative(_votes, min_alts)
+
+        _alts = _alts - min_alts  # remove the dropped alts
+
+        if verbose:
+            print_recap(p_scores, min_alts, round)
+        round += 1
+
+    if verbose:
+        print("===== STV term, history: =====")
+        pprint(_alts_hist)
+        print("==============================")
+
+    return _alts_hist[-1]
+
+
+if __name__ == "__main__":
+    # votes = extract_data()
+    # print(f"winner: {stv_computations(votes, 11, printing=True)}")
+
+    # votes0 = extract_data("./dataset.txt")
+    # print(f"winner: {stv_computations(votes0, 5, printing=True)}")
+
+    print("----- City council -----")
+    votes = extract_data("./dataset_revised.txt")
+    print("city-council winner:", stv(votes, verbose=True))
+
+    print("\n\n----- Mayor -----")
+    votes2 = extract_data("./dataset.txt")
+    print("mayor winner:", stv(votes2, verbose=True))
--- test_stv.py
+++ test_stv.py
@@ -1,36 +1,208 @@
+import STVComputations as stv
 from STVComputations import Profile, stv_computations
 
-def test_simple_case():
-    votes = []
-
-    votes.append(Profile([[1], [2], [3], [4]], 10))
-
-    assert stv_computations(votes=votes, nr_of_alt=4, printing=False) == [1]
+import unittest
 
 
-def test_simple_tie_case():
-    votes = []
+class TestBallotFormat(unittest.TestCase):
+    """Test cases for parsing a comma-sep lin order into a List[List[int]]"""
 
-    votes.append(Profile([[1], [2], [3], [4]], 10))
-    votes.append(Profile([[2], [1], [3], [4]], 10))
+    cases = {
+        "basic": (
+            "1,2,3,4",
+            [[1], [2], [3], [4]],
+        ),
+        "tied_0": (
+            "1,2,3,{4,5}",
+            [[1], [2], [3], [4, 5]],
+        ),
+        "tied_1": (
+            "{1,2},3,4,5",
+            [[1, 2], [3], [4], [5]],
+        ),
+    }
 
-    assert stv_computations(votes=votes, nr_of_alt=4, printing=False) == [1, 2]
+    def test_format(self):
+        for name, (inp, exp) in self.cases.items():
+            out = stv.format_ballot(inp)
+            self.assertListEqual(out, exp)
 
 
-def test_square_bracket():
-    votes = []
+class TestProfileParsing(unittest.TestCase):
 
-    votes.append(Profile([[1], [2], [3], [4]], 10))
-    votes.append(Profile([[2], [1], [3], [4]], 10))
-    votes.append(Profile([[3], [4, 2], [1]], 5))
+    lines = [
+        "2: 1,2,3,4",
+        "5: 1,2,3,{4,5}",
+    ]
 
-    assert stv_computations(votes=votes, nr_of_alt=4, printing=False) == [2]
+    outs = [
+        Profile([[1], [2], [3], [4]], 2),
+        Profile([[1], [2], [3], [4, 5]], 5),
+    ]
+
+    def test_parsing(self):
+        for line, expected in zip(self.lines, self.outs):
+            p = stv.line_extract(line)
+            self.assertEqual(p, expected)
 
 
-def test_tie_as_top_vote():
-    votes = []
-    votes.append(Profile([[1], [3], [4]], 10))
-    votes.append(Profile([[2], [3], [4]], 10))
-    votes.append(Profile([[2, 3], [1], [4]], 10))
+class TestPluralityRounds(unittest.TestCase):
 
-    assert stv_computations(votes=votes, nr_of_alt=4, printing=True) == [2]
+    cases = {
+        "simple": (
+            [Profile([[1], [2], [3]], 3)],
+            {
+                1: 3,
+                2: 0,
+                3: 0,
+            },
+        ),
+        "tied": (
+            [
+                Profile([[1], [2], [3]], 2),
+                Profile([[2], [1], [3]], 2),
+            ],
+            {
+                1: 2,
+                2: 2,
+                3: 0,
+            },
+        ),
+        "split": (
+            [Profile([[1, 2], [3]], 1)],
+            {
+                1: 0.5,
+                2: 0.5,
+                3: 0,
+            },
+        ),
+    }
+
+    def test_plur_round(self):
+        for name, (profs, exp) in self.cases.items():
+            alts = stv.all_alts(profs)
+            res = stv.plurality_round(profs, alts)
+            self.assertDictEqual(res, exp)
+
+
+class TestRemoveAlts(unittest.TestCase):
+    cases = {
+        # basic case, deletes a lin order cell
+        "a": {
+            "from": [Profile([[1], [2]], 2)],
+            "rm": {2},
+            "out": [Profile([[1]], 2)],
+        },
+        # multiple case
+        "b": {
+            "from": [
+                Profile([[1], [2, 3]], 3),
+                Profile([[2, 3], [1]], 1),
+            ],
+            "rm": {2, 3},
+            "out": [
+                Profile([[1]], 3),
+                Profile([[1]], 1),
+            ],
+        },
+        # one in a tie
+        "c": {
+            "from": [Profile([[1], [2, 3]], 1)],
+            "rm": {3},
+            "out": [Profile([[1], [2]], 1)],
+        },
+        # elim lin ord completely
+        "d": {
+            "from": [
+                Profile([[1], [2]], 1),
+                Profile([[2]], 1),
+            ],
+            "rm": {2},
+            "out": [Profile([[1]], 1)],
+        },
+    }
+
+    def test_rm_alts(self):
+        for name, spec in self.cases.items():
+            votes = spec["from"]
+            rm_alts = spec["rm"]
+            expected = spec["out"]
+            res = stv.remove_alternative(votes, rm_alts)
+            self.assertEqual(res, expected)
+
+
+class TestSTVCompute(unittest.TestCase):
+
+    cases = {
+        "simple": {
+            "in": [Profile([[1], [2], [3], [4]], 10)],
+            "out": {1},
+        },
+        "out_tie": {
+            "in": [
+                Profile([[1], [2], [3], [4]], 10),
+                Profile([[2], [1], [3], [4]], 10),
+            ],
+            "out": {1, 2},
+        },
+        "lin_ord_equiv": {
+            "in": [
+                Profile([[1], [2], [3], [4]], 10),
+                Profile([[2], [1], [3], [4]], 10),  # this would tie {1,2} as prev. case
+                Profile([[3], [4, 2], [1]], 5),  # this breaks in favour of {2}
+            ],
+            "out": {2},
+        },
+        "lin_ord_eq_top": {
+            "in": [
+                Profile([[1], [3], [4]], 10),
+                Profile([[2], [3], [4]], 10),  # this would tie {1,2} as prev. case
+                Profile([[2, 3], [1], [4]], 10),  # this breaks in favour of {2}
+            ],
+            "out": {2},
+        },
+    }
+
+    def test_stv(self):
+        for name, spec in self.cases.items():
+            inp = spec["in"]
+            expected = spec["out"]
+            res = stv.stv(inp)
+            self.assertSetEqual(res, expected)
+
+            # check consistency with prev implem
+            p_res = stv.stv_computations(inp, len(stv.all_alts(inp)), printing=False)
+
+            self.assertSetEqual(set(p_res), expected)
+
+
+class TestSTVFromFile(unittest.TestCase):
+    cases = {
+        "city-council": {
+            "file": "./dataset_revised.txt",
+            "expected": {8},
+        },
+        "mayor": {
+            "file": "./dataset.txt",
+            "expected": {4},
+        },
+    }
+
+    def test_from_file(self):
+        for name, spec in self.cases.items():
+            votes = stv.extract_data(spec["file"])
+            res = stv.stv(votes)
+
+            self.assertSetEqual(res, spec["expected"])
+
+            # check consistency with prev implem
+
+            p_res = stv.stv_computations(
+                votes, len(stv.all_alts(votes)), printing=False
+            )
+
+            self.assertSetEqual(set(p_res), spec["expected"])
+
+
+if __name__ == "__main__":
+    unittest.main()
